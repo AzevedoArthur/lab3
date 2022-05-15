aff00f13af2da130f381a968ffed323ace2a4480
Arthur Azevedo Lima <azevedo.arthur@discente.ufma.br>
2022-05-15 16:06:26 -0300

    Todos os testes para Cargo funcionando

--- egresso/src/test/java/com/egresso/egresso/model/repositories/CargoRepositoryTest.java
+++ egresso/src/test/java/com/egresso/egresso/model/repositories/CargoRepositoryTest.java
@@ -1,5 +1,8 @@
 package com.egresso.egresso.model.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.egresso.egresso.model.entities.Cargo; // Interface
 
 import org.junit.jupiter.api.Assertions;
@@ -18,30 +21,70 @@ public class CargoRepositoryTest{
     CargoRepository repository;
     
     @Test
-    public void deveSalvarCargos(){
+    public void deveVerificarSalvarCargos() {
         // Cenário
-        Cargo cargo = Cargo.builder().nome("Gerente").descricao("Faz alguma coisa X.").build();
+        Cargo cargo_a_salvar = Cargo.builder().nome("Gerente").descricao("Faz alguma coisa X.").build();
 
         // Ação - operar no banco
-        Cargo salvo = repository.save(cargo);
+        Cargo salvo = repository.save(cargo_a_salvar);
 
         // Verificação - A ação ocorreu?
    
         Assertions.assertNotNull(salvo);
-        Assertions.assertEquals(cargo.getNome(), salvo.getNome());
-        Assertions.assertEquals(cargo.getDescricao(), salvo.getDescricao());
-
+        Assertions.assertEquals(cargo_a_salvar.getNome(), salvo.getNome());
+        Assertions.assertEquals(cargo_a_salvar.getDescricao(), salvo.getDescricao());
     }
     
-    // @Test
-    // public void deveObterCargos(){
-    //     // Ação - operar no banco
-    //     Cargo salvo = repository.save(cargo);
+    @Test
+    public void deveVerificarObterTodosCargos(){
+        // Ação - operar no banco
+        List<Cargo> query = repository.findAll();
 
-    //     // Verificação - A ação ocorreu?
+        // Verificação - A ação ocorreu?
    
-    //     Assertions.assertNotNull(salvo);
-    //     Assertions.assertEquals(cargo.getNome(), salvo.getNome());
+        Assertions.assertNotNull(query);
+        Assertions.assertFalse(query.isEmpty());
+    }
+    
+    @Test
+    public void deveVerificarObterCargoSalvo(){
+        // Ação - operar no banco
+        Cargo cargo_a_salvar = Cargo.builder().nome("Gerente").descricao("Faz alguma coisa X.").build();
 
-    // }
+        // Ação - operar no banco
+        Cargo salvo = repository.save(cargo_a_salvar);
+   
+        Assertions.assertNotNull(salvo);
+        Assertions.assertEquals(cargo_a_salvar.getNome(), salvo.getNome());
+        Assertions.assertEquals(cargo_a_salvar.getDescricao(), salvo.getDescricao());
+
+        Optional<Cargo> query = repository.findById(salvo.getId());
+
+        // Verificação - A ação ocorreu?
+   
+        Assertions.assertNotNull(query);
+        Assertions.assertFalse(query.isEmpty());
+        Cargo q = query.get();
+        Assertions.assertEquals(salvo.getNome(), q.getNome());
+        Assertions.assertEquals(salvo.getDescricao(), q.getDescricao());
+    }
+    
+    @Test
+    public void deveVerificarRemoverCargoSalvo(){
+        // Ação - operar no banco
+        Cargo cargo_a_salvar = Cargo.builder().nome("Gerente").descricao("Faz alguma coisa X.").build();
+
+        // Ação - operar no banco
+        Cargo salvo = repository.save(cargo_a_salvar);
+   
+        Assertions.assertNotNull(salvo);
+        Assertions.assertEquals(cargo_a_salvar.getNome(), salvo.getNome());
+        Assertions.assertEquals(cargo_a_salvar.getDescricao(), salvo.getDescricao());
+
+        repository.deleteById(salvo.getId());
+        Optional<Cargo> query = repository.findById(salvo.getId());
+        // Verificação - A ação ocorreu?
+   
+        Assertions.assertTrue(query.isEmpty());
+    }
 }
