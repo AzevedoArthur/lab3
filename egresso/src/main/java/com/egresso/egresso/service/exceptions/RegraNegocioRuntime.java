acdb8ef1ef03d5fc5ce73182e8cd6a91380160d6
Arthur Azevedo Lima <azevedo.arthur@discente.ufma.br>
2022-05-31 16:32:28 -0300

    Exception criada

---
+++ egresso/src/main/java/com/egresso/egresso/service/exceptions/RegraNegocioRuntime.java
@@ -0,0 +1,7 @@
+package com.egresso.egresso.service.exceptions;
+
+public class RegraNegocioRuntime extends RuntimeException {
+    public RegraNegocioRuntime(String msg) {
+        super(msg);
+    }
+}
